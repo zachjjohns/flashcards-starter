8c5de35d96dee313f03a890c2e143624ce921dbe
Zach Johnson <zachjjohns@gmail.com>
2021-03-23 12:55:50 -0500

    Add countCard method and test for Deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -15,7 +15,16 @@ describe("Deck", function() {
     const card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
     const card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
     const card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"]);
-
     const deck = new Deck([card1, card2, card3]);
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+  it("should be able to count the number of cards in the deck", function() {
+    const card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
+    const card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
+    const card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"]);
+    const deck = new Deck([card1, card2, card3]);
+    deck.countCards();
+    expect(deck.countCards()).to.equal(3);
   });
 });
