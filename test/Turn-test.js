caf97602a49eb1fe093c71fc7c9d45771c0a1f0a
Zach Johnson <zachjjohns@gmail.com>
2021-03-22 19:16:33 -0500

    Add tests for Turn-test.js and updates Turn.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,2 +1,27 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+const Turn = require('../src/Turn');
+
+describe("Turn", function() {
+
+  it("should be a function", function() {
+    // const turn = new Turn();
+    expect(Turn).to.be.a("function");
+  });
+
+  it("should be an instance of Turn", function() {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it("should store the user's guess to the question", function() {
+    const turn = new Turn("flamingo", "What's the best pink, long-legged bird?");
+    expect(turn.guess).to.equal("flamingo")
+  });
+
+  it("should store a Card object for the current card in play", function() {
+    const turn = new Turn("flamingo", "What's the best pink, long-legged bird?");
+    expect(turn.card).to.equal("What's the best pink, long-legged bird?");
+  });
+});
