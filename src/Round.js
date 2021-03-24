2ce9cf88eef71d1607d25c3a3d2bea9cb2e6e908
Zach Johnson <zachjjohns@gmail.com>
2021-03-24 15:12:52 -0500

    Add takeTurn function+test

--- src/Round.js
+++ src/Round.js
@@ -1,14 +1,18 @@
+const Turn = require("./Turn");
+
 class Round {
   constructor(deck) {
     this.deck = deck;
+    this.currentCard = this.deck.cards[0];
     this.turns = 0;
   }
 
   returnCurrentCard() {
-    return this.deck.cards[0];
+    return this.currentCard;
   }
 
   takeTurn(guess) {
+    let turn = new Turn(guess);
 
   }
 }
--- test/Round-test.js
+++ test/Round-test.js
@@ -2,56 +2,44 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Card = require('../src/Card');
-const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
+const Turn = require('../src/Turn');
 const Round = require('../src/Round');
 
 describe("Round", () => {
-  let card1, card2, card3, deck, round;
+  let card1, card2, card3, deck, turn, round;
   beforeEach(() => {
     card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
     card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
     card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"]);
     deck = new Deck([card1, card2, card3]);
+    turn = new Turn("flamingo", card1);
     round = new Round(deck);
   });
 
-  it("should be a function", function() {
+  it("should be a function", () => {
     expect(Round).to.be.a("function");
   });
 
-
-  it("should be an instance of Round", function() {
-    // const round = new Round();
+  it("should be an instance of Round", () => {
     expect(round).to.be.an.instanceof(Round);
   });
 
-  it("should instantiate with a deck of cards", function() {
-    // const card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
-    // const card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
-    // const card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"]);
-    // const deck = new Deck([card1, card2, card3]);
-    // const round = new Round(deck);
+  it("should instantiate with a deck of cards", () => {
     expect(round.deck).to.equal(deck);
   });
 
-  it("should be able to return the current card", function() {
-    // const card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
-    // const card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
-    // const card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"], "philosophy");
-    // const deck = new Deck([card1, card2, card3]);
-    // const round = new Round(deck);
+  it("should start with 0 turns", () => {
+    expect(round.turns).to.equal(0);
+  })
+
+  it("should be able to return the current card", () => {
     round.returnCurrentCard();
     expect(round.returnCurrentCard()).to.equal(card1);
   });
 
-  // it("should start a new Turn when a guess is made", function() {
-    // const card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
-    // const card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
-    // const card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"], "philosophy");
-    // const deck = new Deck([card1, card2, card3]);
-    // const round = new Round(deck);
-    // round.takeTurn("flamingo");
-    // expect
-  // })
+  it("should start a new Turn when a guess is made", () => {
+    round.takeTurn("flamingo");
+    expect(turn).to.be.an.instanceof(Turn);
+  })
 })
