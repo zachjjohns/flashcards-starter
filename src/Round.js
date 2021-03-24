4e90292856515b625b375c98fba00f2db97be89f
Zach Johnson <zachjjohns@gmail.com>
2021-03-24 09:17:30 -0500

    Add returnCurrentCard function + test

--- src/Round.js
+++ src/Round.js
@@ -2,6 +2,11 @@ class Round {
   constructor(deck) {
     this.deck = deck;
   }
+
+  returnCurrentCard() {
+    console.log(this.deck.cards);
+    return this.deck.cards[0];
+  }
 }
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -7,5 +7,31 @@ const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
 describe("Round", function() {
-  
+  it("should be a function", function() {
+    expect(Round).to.be.a("function");
+  });
+
+  it("should be an instance of Round", function() {
+    const round = new Round();
+    expect(round).to.be.an.instanceof(Round);
+  });
+
+  it("should instantiate with a deck of cards", function() {
+    const card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
+    const card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
+    const card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"]);
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.deck).to.equal(deck);
+  });
+
+  it("should be able to return the current card", function() {
+    const card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
+    const card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
+    const card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"], "philosophy");
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    round.returnCurrentCard();
+    expect(round.returnCurrentCard()).to.equal(card1);
+  })
 })
