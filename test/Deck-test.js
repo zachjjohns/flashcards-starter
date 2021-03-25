7eba89149c72cae5425913a025dd4381aa37624b
Zach Johnson <zachjjohns@gmail.com>
2021-03-25 15:07:28 -0500

    update Deck and Turn tests with beforeEach

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,24 +6,23 @@ const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
 
 describe("Deck", function() {
+  let card1, card2, card3, deck;  
+  beforeEach(() => {
+    card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
+    card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
+    card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"]);
+    deck = new Deck([card1, card2, card3]);
+  });
 
   it("should be a function", function() {
     expect(Deck).to.be.a("function");
   });
 
   it("should instantiate with an array of cards", function() {
-    const card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
-    const card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
-    const card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"]);
-    const deck = new Deck([card1, card2, card3]);
     expect(deck).to.be.an.instanceof(Deck);
   });
 
   it("should be able to count the number of cards in the deck", function() {
-    const card1 = new Card(3, "What's the best pink, long-legged bird?", ["pigeon", "penguin", "flamingo"], "flamingo");
-    const card2 = new Card(6, "When did Facebook launch?", ["2004", "1998", "2008"], "2004");
-    const card3 = new Card(19, "A doctor with a PhD is a doctor of what?", ["philosophy", "science", "math"]);
-    const deck = new Deck([card1, card2, card3]);
     deck.countCards();
     expect(deck.countCards()).to.equal(3);
   });
